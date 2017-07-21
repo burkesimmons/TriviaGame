693d1841433e37047f4ceaf584e46725ee4b1601
Burke Simmons <burkesimmons@gmail.com>
2017-07-20 18:29:04 -0600

    Got help from Jason and he had me code with him a bunch of new stuff at the end of index.html, but still need to get it working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,12 @@
-$(document).ready(function() {
-	// alert('yes');
-//  Interval Demonstration
+$(function() {
+	//  Interval Demonstration
     //  Set our number counter to 100.
     var number = 30;
-
     //  Variable that will hold our interval ID when we execute
     //  the "run" function
     var intervalId;
-
     //  When the stop button gets clicked, run the stop function.
     $("#start").on("click", run);
-
-    //  When the resume button gets clicked, execute the run function.
-    // $("#resume").on("click", run);
-
     //  The run function sets an interval
     //  that runs the decrement function once a second.
     function run() {
@@ -21,82 +14,8 @@ $(document).ready(function() {
       listQuestions ();
     }
 
-    var questions = [];
-    questions[0] = 'Which beast was the most subltle of all beasts?';
-    questions[1] = 'What is the world\'s most common religion?',
-    questions[2] = 'What is the second largest country (in size) in the world?';
+    var score = {};
 
-    var choices = [];
-    choices[0] = ['Cow', 'Snake', 'Iguana', 'Centipede'],
-    choices[1] = ['Christianity', 'Buddhism', 'Hinduism', 'Islam'],
-    choices[2] = ['USA', 'China', 'Canada', 'Russia'];
-
-    var answers = [];
-    answers[0] = ['Some difference'],
-    answers[1] = ['Christianity'],
-    answers[2] = ['Canada'];
-
-    var score = 0;
-
-    function listQuestions () {
-    	for (var i = 0; i < questions.length; i++) {
-			$('#question').append('<h3>'+questions[i]+'</h3>');
-			var choicesOutput=[];
-			for (var k = 0; k < choices[i].length; k++){
-				choicesOutput.push('<p><input type = "radio" name = "questionchoice">' + choices[i][k] + '</p>');
-    	        $('#choices').append(choicesOutput.join(""));
-    	        $('#button').append('<p><button onClick = "getRadioValue()">Check</button></p><br>');
-    	        };
-    	        
-    	    }
-    	};
-    
-
-    //  The decrement function.
-    function decrement() {
-
-      //  Decrease number by one.
-      number--;
-
-      //  Show the number in the #show-number tag.
-      $("#show-number").html("<h2>" + 'Time remaining: ' + number + ' Seconds' + "</h2>");
-
-
-      //  Once number hits zero...
-      if (number === 0) {
-
-        //  ...run the stop function.
-        stop();       
-      }
-    }
-
-    //  The stop function
-    function stop() {
-
-      //  Clears our intervalId
-      //  We just pass the name of the interval
-      //  to the clearInterval function.
-      clearInterval(intervalId);
-    }//  Interval Demonstration
-    //  Set our number counter to 100.
-    var number = 30;
-
-    //  Variable that will hold our interval ID when we execute
-    //  the "run" function
-    var intervalId;
-
-    //  When the stop button gets clicked, run the stop function.
-    $("#start").on("click", run);
-
-    //  When the resume button gets clicked, execute the run function.
-    // $("#resume").on("click", run);
-
-    //  The run function sets an interval
-    //  that runs the decrement function once a second.
-    function run() {
-      intervalId = setInterval(decrement, 1000);
-      listQuestions ();
-    }
 
     var questions = [];
     questions[0] = 'Which beast was the most subltle of all beasts?';
@@ -113,17 +32,19 @@ $(document).ready(function() {
     answers[1] = ['4'],
     answers[2] = ['Jacob'];
 
-    var score = 0;
 
     function listQuestions () {
-    	for (var i = 0; i < questions.length; i++) {
-			$('#question').append('<h3>'+questions[i]+'</h3>');
+    	if (var i = 0; i < questions.length) {
+			$('#question').append('<h3>' + questions[i] + '</h3>');
 			var choicesOutput=[];
+			$('#choices').html(choicesOutput.join(""));
+			console.log(choicesOutput);
 			for (var k = 0; k < choices[i].length; k++){
 				choicesOutput.push('<p><input type = "radio" name = "questionchoice">' + choices[i][k] + '</p>');
-				
+				console.log(choicesOutput);
     	        };
-    	        
+    	        $('#choices').html(choicesOutput.join(""));
+    	        console.log(choicesOutput);
     	        $('#button').append('<p><button onClick = "getRadioValue()">Check</button></p><br>');
     	    }
     	};
@@ -155,7 +76,46 @@ $(document).ready(function() {
       //  to the clearInterval function.
       clearInterval(intervalId);
     }
-});
 
-    //  Execute the run function.
-    // run();
+    var questions = [
+    	{
+    		id: 1,
+    		text: 'What is my birthday',
+    		answers: [
+    			{
+    				text: 'Jan 2',
+    				isCorrect: false,
+    			}
+    			{
+    				text: 'Jan 1',
+    				isCorrect: true,
+    			}
+    		]
+    	},
+    	{
+    		id: 2,
+    		text: 'What is my dad\'s birthday?',
+    		answers: [
+    		]
+    	},
+    	state: 'unanswered'
+    }]
+
+    var score = {};
+
+    var questionsElement = $('.questions');
+    var listWrapper = $('<ul>');
+    questions.forEach(function(currentQuestion,index) {
+    	var listElement = $('<li>');
+    	listElement.html(currentQuestion.text);
+    	var answersWrapper = $('<ul>');
+    	currentQuestion.answers.forEach(function(currentAnswer) {
+    		var anserListItem = $('<li>');
+    		answerListItem.html('input type="radio" value="' + currentAnswer.text + '"name')
+    		currentQuestion.id + '"' + currentAnswer.text + 
+    	})
+    	listWrapper.append(listElement)
+    });
+    questionsElement.append(listWrapper);
+
+});
